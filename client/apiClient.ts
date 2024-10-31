24df80073f79d0ca30e4228e5258fac3bbf21adb
faelyn-jpg <169906179+faelyn-jpg@users.noreply.github.com>
2024-11-01 08:53:44 +1300

    add getCategories API request function

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -5,3 +5,8 @@ export async function getQuiz(): Promise<QuizQuestion> {
   const res = await request.get('https://opentdb.com/api.php?amount=10')
   return res.body as QuizQuestion
 }
+
+export async function getCategories() {
+  const res = await request.get('https://opentdb.com/api_category.php')
+  return res.body 
+}
