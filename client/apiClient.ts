e1b0533928bfcb4cb0410518c4327fc6bd399202
Tyaan Singh <20968808+tyaan@users.noreply.github.com>
2024-11-01 09:08:00 +1300

    adds parameters to placeholder API function

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -1,7 +1,7 @@
 import request from 'superagent'
 import { QuizQuestion } from '../models/quiz'
 
-export async function getQuiz(): Promise<QuizQuestion> {
+export async function getQuiz(numQuestions: number, category: string, difficulty: string): Promise<QuizQuestion> {
   const res = await request.get('https://opentdb.com/api.php?amount=10')
   return res.body as QuizQuestion
 }
